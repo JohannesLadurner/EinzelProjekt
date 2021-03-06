9ae2e8756c556c43e9125629f96095b94c36ed4e
Johannes Ladurner <joladurner@edu.aau.at>
2021-03-06 16:11:10 +0100

    added Prime numbers check

--- app/src/main/java/com/example/einzelprojekt/MainActivity.java
+++ app/src/main/java/com/example/einzelprojekt/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     Button sendButton;
     EditText msgForServer;
     TextView msgServer;
+    TextView msgPrimes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         sendButton = findViewById(R.id.BTNSend);
         msgForServer = findViewById(R.id.InputField);
         msgServer = findViewById(R.id.TVServerRespond);
+        msgPrimes = findViewById(R.id.TVPrime);
     }
 
     public void BTNSend_Click(View v) throws InterruptedException {
@@ -27,6 +29,30 @@ public class MainActivity extends AppCompatActivity {
         new Thread(client).start();
         Thread.sleep(100);
         msgServer.setText(client.getServerMessage());
+        printAllPrimeDigits(msgForServer.getText().toString());
+    }
+
+    public void printAllPrimeDigits(String number)
+    {
+        String msg = "";
+        for (int i = 0; i < number.length(); i++) {
+            int digit = number.charAt(i) - '0';
+            if(digit > 1 && isPrime(digit))
+            {
+                msg += digit;
+            }
+        }
+        msgPrimes.setText("Prime Numbers: " + msg);
+    }
+    public boolean isPrime(int number)
+    {
+        for (int i = 2; i <= number/2; i++) {
+            if(number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
